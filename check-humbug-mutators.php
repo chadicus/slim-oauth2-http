72899dfdafa22cc3ad170292e6be252236026061
chadicus <chadwickgray@gmail.com>
2016-11-14 11:14:08 -0500

    Add humbug back to travis build

--- check-humbug-mutators.php
+++ check-humbug-mutators.php
@@ -1,15 +1,17 @@
 #!/usr/bin/env php
 <?php
 
-call_user_func(function () {
-    echo file_get_contents(__DIR__ . '/humbuglog.txt');
+echo file_get_contents(__DIR__ . '/humbuglog.txt');
 
-    $log = json_decode(file_get_contents(__DIR__ . '/humbuglog.json'), true);
-    if (!array_key_exists('escapes', $log['summary'])) {
-        throw new \UnexpectedValueException('Could not find "escapes" in summary');
-    }
+$log = json_decode(file_get_contents(__DIR__ . '/humbuglog.json'), true);
+if (!array_key_exists('escapes', $log['summary'])) {
+    echo "Could not find 'escapes' in summary\n";
+    exit(1);
+}
 
-    if ($log['summary']['escapes']) {
-        throw new \UnexpectedValueException('Humbug reported escaped mutants');
-    }
-});
+if ($log['summary']['escapes']) {
+    echo "Humbug reported escaped mutants\n";
+    exit(1);
+}
+
+exit(0);
